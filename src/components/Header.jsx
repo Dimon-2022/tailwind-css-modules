8300bd9c0f698b944420984bb6eb83c5c0b11354
dima.moisieienko <dimonmail09@gmail.com>
2025-05-18 17:12:03 +0300

    connected tailwind CSS framework

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,19 @@ import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 
 function Header() {
-
-console.log(styles);
-
   return (
-    <>
-      <h1 className={styles.dima}>Header</h1>
+    <header className="bg-blue-500">
+      {/* <h1 className={styles.dima}>Header</h1> */}
       {/* <a href="http://localhost:5173/about">about</a> */}
+
+      <h1 className="text-4xl text-violet-900/50 hover:text-violet-900/100">
+        Header
+      </h1>
       <NavLink to={"/"}>Home</NavLink>
       <NavLink to={"/about"}>About</NavLink>
       <NavLink to={"/cart"}>Cart</NavLink>
       <hr />
-    </>
+    </header>
   );
 }
 
