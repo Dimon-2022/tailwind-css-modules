4e7bf37384d3e71873a7aa913142ee41cc27197b
dima.moisieienko <dimonmail09@gmail.com>
2025-05-19 19:01:01 +0300

    tailwind CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,33 @@ import styles from "./Header.module.css";
 
 function Header() {
   return (
-    <header className="bg-blue-500">
+    <header className="flex justify-between px-5 py-8 bg-blue-200 shadow-md bg-5">
+      <img className="h-6" src="/logo.svg" alt="logo"></img>
       {/* <h1 className={styles.dima}>Header</h1> */}
       {/* <a href="http://localhost:5173/about">about</a> */}
 
       <h1 className="text-4xl text-violet-900/50 hover:text-violet-900/100">
         Header
       </h1>
-      <NavLink to={"/"}>Home</NavLink>
-      <NavLink to={"/about"}>About</NavLink>
-      <NavLink to={"/cart"}>Cart</NavLink>
-      <hr />
+      <nav>
+        <ul className="flex gap-14">
+          <li className="inline mb-4">
+            <NavLink className="font-semibold " to={"/"}>
+              Home
+            </NavLink>
+          </li>
+          <li className="inline">
+            <NavLink className="font-semibold" to={"/about"}>
+              About
+            </NavLink>
+          </li>
+          <li className="inline">
+            <NavLink className="font-semibold" to={"/cart"}>
+              Cart
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,15 @@ function Home() {
   console.log(location);
 
   return (
-    <div>
-      <h1>Categories</h1>
-      <ul style={{ display: "flex" }}>
+    <div className="py-10">
+      <h1 className="mb-8 text-2xl font-semibold text-center ">Categories</h1>
+      <ul className="grid grid-cols-3 gap-4 px-5">
         {categories.map((category) => (
           <li key={category.id}>
-            <Link to={`/category/${category.name}`}>
-              {category.name}
-              <img src={category.img} alt={category.name} style={{ width: "150px" }} />
+            <Link className="relative flex flex-col items-center justify-center" to={`/category/${category.name}`}>
+              <span className="absolute z-10 text-xl font-semibold text-white">{category.name}</span>
+              <img src={category.img} alt={category.name} className="rounded-md" />
+              <div className="absolute inset-0 rounded-md bg-gray-950 opacity-40 "></div>
             </Link>
           </li>
         ))}
